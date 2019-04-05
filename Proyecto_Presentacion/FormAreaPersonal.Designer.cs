3ecd51ba5d8c3581266ed5061097791e6c4425e1
Valentín Sánchez Boto <valentinsabo5@gmail.com>
2019-04-05 21:35:10 +0200

    AreaPersonal

--- Proyecto_Presentacion/FormAreaPersonal.Designer.cs
+++ Proyecto_Presentacion/FormAreaPersonal.Designer.cs
@@ -29,7 +29,14 @@
         private void InitializeComponent()
         {
             this.graficoBarras = new LiveCharts.WinForms.CartesianChart();
-            this.button1 = new System.Windows.Forms.Button();
+            this.panelCabecera = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSegundo = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPrimero = new System.Windows.Forms.Button();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.panelCabecera.SuspendLayout();
+            this.panelContenedor.SuspendLayout();
             this.SuspendLayout();
             // 
             // graficoBarras
@@ -38,21 +45,88 @@
             this.graficoBarras.Location = new System.Drawing.Point(0, 0);
             this.graficoBarras.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
             this.graficoBarras.Name = "graficoBarras";
-            this.graficoBarras.Size = new System.Drawing.Size(959, 644);
+            this.graficoBarras.Size = new System.Drawing.Size(959, 604);
             this.graficoBarras.TabIndex = 0;
             this.graficoBarras.Text = "cartesianChart1";
             this.graficoBarras.ChildChanged += new System.EventHandler<System.Windows.Forms.Integration.ChildChangedEventArgs>(this.cartesianChart1_ChildChanged);
             // 
-            // button1
+            // panelCabecera
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button1.Location = new System.Drawing.Point(0, 621);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(959, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panelCabecera.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.panelCabecera.Controls.Add(this.panel2);
+            this.panelCabecera.Controls.Add(this.btnSegundo);
+            this.panelCabecera.Controls.Add(this.panel1);
+            this.panelCabecera.Controls.Add(this.btnPrimero);
+            this.panelCabecera.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCabecera.Location = new System.Drawing.Point(0, 0);
+            this.panelCabecera.Name = "panelCabecera";
+            this.panelCabecera.Size = new System.Drawing.Size(959, 40);
+            this.panelCabecera.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(39)))));
+            this.panel2.Location = new System.Drawing.Point(152, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 40);
+            this.panel2.TabIndex = 21;
+            // 
+            // btnSegundo
+            // 
+            this.btnSegundo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSegundo.FlatAppearance.BorderSize = 0;
+            this.btnSegundo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(39)))));
+            this.btnSegundo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(55)))), ((int)(((byte)(34)))));
+            this.btnSegundo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSegundo.Font = new System.Drawing.Font("Segoe UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSegundo.ForeColor = System.Drawing.Color.White;
+            this.btnSegundo.Image = global::Proyecto_Presentacion.Properties.Resources.iconocurso;
+            this.btnSegundo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSegundo.Location = new System.Drawing.Point(152, 0);
+            this.btnSegundo.Name = "btnSegundo";
+            this.btnSegundo.Size = new System.Drawing.Size(170, 40);
+            this.btnSegundo.TabIndex = 20;
+            this.btnSegundo.Text = "Segundo curso";
+            this.btnSegundo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSegundo.UseVisualStyleBackColor = true;
+            this.btnSegundo.Click += new System.EventHandler(this.btnSegundo_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(39)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 40);
+            this.panel1.TabIndex = 19;
+            // 
+            // btnPrimero
+            // 
+            this.btnPrimero.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPrimero.FlatAppearance.BorderSize = 0;
+            this.btnPrimero.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(39)))));
+            this.btnPrimero.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(55)))), ((int)(((byte)(34)))));
+            this.btnPrimero.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrimero.Font = new System.Drawing.Font("Segoe UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrimero.ForeColor = System.Drawing.Color.White;
+            this.btnPrimero.Image = global::Proyecto_Presentacion.Properties.Resources.iconocurso;
+            this.btnPrimero.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPrimero.Location = new System.Drawing.Point(0, 0);
+            this.btnPrimero.Name = "btnPrimero";
+            this.btnPrimero.Size = new System.Drawing.Size(152, 40);
+            this.btnPrimero.TabIndex = 18;
+            this.btnPrimero.Text = "Primer curso";
+            this.btnPrimero.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnPrimero.UseVisualStyleBackColor = true;
+            this.btnPrimero.Click += new System.EventHandler(this.btnPrimero_Click);
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.Controls.Add(this.graficoBarras);
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenedor.Location = new System.Drawing.Point(0, 40);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(959, 604);
+            this.panelContenedor.TabIndex = 2;
             // 
             // FormAreaPersonal
             // 
@@ -60,12 +134,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(959, 644);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.graficoBarras);
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.panelCabecera);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormAreaPersonal";
             this.Text = "FormAreaPersonal";
+            this.panelCabecera.ResumeLayout(false);
+            this.panelContenedor.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -73,6 +149,11 @@
         #endregion
 
         private LiveCharts.WinForms.CartesianChart graficoBarras;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelCabecera;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSegundo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPrimero;
+        private System.Windows.Forms.Panel panelContenedor;
     }
 }
--- Proyecto_Presentacion/FormAreaPersonal.cs
+++ Proyecto_Presentacion/FormAreaPersonal.cs
@@ -2,131 +2,93 @@
 using LiveCharts.Wpf;
 using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 
 namespace Proyecto_Presentacion
 {
+    
     public partial class FormAreaPersonal : Form
     {
+        List<double> notasPrimero = new List<double> { 1, 2, 3, 4, 5, 6 };
+        List<double> notasSegundo = new List<double> { 1, 2, 3, 4, 5, 6, 7 };
         public FormAreaPersonal()
         {
+            
             InitializeComponent();
             DoubleBuffered = true;
-
+            this.btnPrimero.BackColor = Color.FromArgb(73, 55, 34);
             // Grafico de barras
             graficoBarras.Series = new SeriesCollection
             {
-                new ColumnSeries
+
+                new RowSeries
                 {
-                    Title = "2015",
-                    Values = new ChartValues<double> { 10, 50, 39, 50 }
+                    Title = "NOTAS",
+                    Values = new ChartValues<double>(notasPrimero)
                 }
             };
 
-            //adding series will update and animate the chart automatically
-            graficoBarras.Series.Add(new ColumnSeries
-            {
-                Title = "2016",
-                Values = new ChartValues<double> { 11, 56, 42 }
-            });
-
-            //also adding values updates and animates the chart automatically
-            graficoBarras.Series[1].Values.Add(48d);
-
-            graficoBarras.AxisX.Add(new Axis
-            {
-                Title = "Sales Man",
-                Labels = new[] { "Maria", "Susan", "Charles", "Frida" }
-            });
 
             graficoBarras.AxisY.Add(new Axis
             {
-                Title = "Sold Apps",
-                LabelFormatter = value => value.ToString("N")
+                Title = "ASIGNATURAS",
+                Labels = new[] { "BASES", "ENTORNOS", "FOL", "MARCAS", "PROGRAMACION", "SISTEMAS" }
             });
 
-            // Gráfico Tarta
-            Func<ChartPoint, string> labelPoint = chartPoint =>
-    string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
 
-            graficoTarta.Series = new SeriesCollection
-            {
-                new PieSeries
-                {
-                    Title = "Maria",
-                    Values = new ChartValues<double> {3},
-                    PushOut = 15,
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                },
-                new PieSeries
-                {
-                    Title = "Charles",
-                    Values = new ChartValues<double> {4},
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                },
-                new PieSeries
-                {
-                    Title = "Frida",
-                    Values = new ChartValues<double> {6},
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                },
-                new PieSeries
-                {
-                    Title = "Frederic",
-                    Values = new ChartValues<double> {2},
-                    DataLabels = true,
-                    LabelPoint = labelPoint
-                }
-            };
-
-            graficoTarta.LegendLocation = LegendLocation.Bottom;
-
-            // Gráfico Apilado
-            graficoApilado.Series = new SeriesCollection
-            {
-                new StackedColumnSeries
-                {
-                    Values = new ChartValues<double> {4, 5, 6, 8},
-                    StackMode = StackMode.Values, // this is not necessary, values is the default stack mode
-                    DataLabels = true
-                },
-                new StackedColumnSeries
-                {
-                    Values = new ChartValues<double> {2, 5, 6, 7},
-                    StackMode = StackMode.Values,
-                    DataLabels = true
-                }
-            };
-
-            //adding series updates and animates the chart
-            graficoApilado.Series.Add(new StackedColumnSeries
-            {
-                Values = new ChartValues<double> { 6, 2, 7 },
-                StackMode = StackMode.Values
-            });
-
-            //adding values also updates and animates
-            graficoApilado.Series[2].Values.Add(4d);
-
-            graficoApilado.AxisX.Add(new Axis
-            {
-                Title = "Browser",
-                Labels = new[] { "Chrome", "Mozilla", "Opera", "IE" },
-                Separator = DefaultAxes.CleanSeparator
-            });
-
-            graficoApilado.AxisY.Add(new Axis
-            {
-                Title = "Usage",
-                LabelFormatter = value => value + " Mill"
-            });
         }
 
         private void cartesianChart1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
         {
 
+        }
+
+        private void btnSegundo_Click(object sender, EventArgs e)
+        {
+
+            this.btnPrimero.BackColor = Color.FromArgb(32, 32, 32);
+            this.btnSegundo.BackColor = Color.FromArgb(73, 55, 34);
+            graficoBarras.AxisY.Clear();
+            graficoBarras.Series = new SeriesCollection
+            {
+                new RowSeries
+                {
+                    Title = "NOTAS",
+                    Values = new ChartValues<double> (notasSegundo)
+                }
+            };
+
+
+            graficoBarras.AxisY.Add(new Axis
+            {
+                Title = "ASIGNATURAS",
+                Labels = new[] { "ACCESO", "INTERFACES", "EIE", "INGLES", "PSP", "PMDM", "SGE" }
+            });
+        }
+
+        private void btnPrimero_Click(object sender, EventArgs e)
+        {
+            
+            this.btnSegundo.BackColor = Color.FromArgb(32, 32, 32);
+            this.btnPrimero.BackColor = Color.FromArgb(73, 55, 34);
+            graficoBarras.AxisY.Clear();
+            graficoBarras.Series = new SeriesCollection
+            {
+                new RowSeries
+                {
+                    Title = "NOTAS",
+                    Values = new ChartValues<double>(notasPrimero)
+                }
+            };
+
+
+            graficoBarras.AxisY.Add(new Axis
+            {
+                Title = "ASIGNATURAS",
+                Labels = new[] { "BASES", "ENTORNOS", "FOL", "MARCAS", "PROGRAMACION", "SISTEMAS" }
+            });
         }
     }
 }
